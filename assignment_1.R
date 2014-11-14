3ca591259d111a8bb745579db1f749f1b421fdf3
intoR <man_abdoli@yahoo.com>
2014-11-14 13:23:16 -0800

    Create assignment_1.R

---
+++ assignment_1.R
@@ -0,0 +1,33 @@
+# Code for testing codes for  Assignment #1
+
+source("pollutantmean.R")
+# Assignment 1 - A: Test Commands
+pollutantmean("specdata", "sulfate", 1:10)
+pollutantmean("specdata", "nitrate", 70:72)
+pollutantmean("specdata", "nitrate", 23)
+
+
+source("complete.R")
+# Assignment 1 - B: Test Commands
+complete("specdata", 1)
+complete("specdata", c(2, 4, 8, 10, 12))
+complete("specdata", 30:25)
+complete("specdata", 3)
+
+
+source("complete.R")
+source("corr.R")
+# Assignment 1 - C: Test Commands
+cr <- corr("specdata", 150)
+head(cr)
+summary(cr)
+cr <- corr("specdata", 400)
+head(cr)
+summary(cr)
+cr <- corr("specdata", 5000)
+summary(cr)
+length(cr)
+cr <- corr("specdata")
+summary(cr)
+length(cr)
+
